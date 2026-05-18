df981ec94c78f611d97ec618d65d27726cc3b8d2
artemB <deniskabrat4@gmail.com>
2026-05-18 22:18:38 +0300

    Initial commit

---
+++ src/main/java/org/example/deepshuffle/DeepshuffleApplication.java
@@ -0,0 +1,13 @@
+package org.example.deepshuffle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepshuffleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeepshuffleApplication.class, args);
+    }
+
+}
